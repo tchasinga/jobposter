9162f855532f1dacb8eafe7e0dc1d07f91831236
tchasinga <tchasingajacques@gmail.com>
2025-04-11 10:29:47 +0300

    feat: update Keeper component styles and add new dependencies for animations

--- app/Pages/Keeper.jsx
+++ app/Pages/Keeper.jsx
@@ -2,10 +2,11 @@
 import React from 'react';
 
 
+
 export default function Keeper() {
   return (
     <div
-      className="relative h-[550px] w-full object-fill max-w-full bg-fixed" // Added relative positioning here
+      className="relative h-[550px] w-full object-fill max-w-full bg-fixed"
       style={{
         background: `linear-gradient(to right, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0)), url(${Oneimg.src}) center / cover no-repeat`,
       }}
