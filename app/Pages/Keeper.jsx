714c5c6b8a0972ed3eb899090dd3caf6a7f3a69f
tchasinga <tchasingajacques@gmail.com>
2025-04-11 10:37:11 +0300

    feat: update Keeper component to use correct image source and integrate into Home component

--- app/Pages/Keeper.jsx
+++ app/Pages/Keeper.jsx
@@ -1,5 +1,10 @@
 "use client";
 import React from 'react';
+import ImgOne from "../Image/ImgOne.jpeg";
+import ImgTwo from "../Image/ImgTwo.jpeg";
+import ImgThree from "../Image/ImgThree.jpeg";
+import ImageFour from "../Image/ImageFour.jpeg";
+import ImgFive from "../Image/Img05.webp";
 
 
 
@@ -8,7 +13,7 @@ export default function Keeper() {
     <div
       className="relative h-[550px] w-full object-fill max-w-full bg-fixed"
       style={{
-        background: `linear-gradient(to right, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0)), url(${Oneimg.src}) center / cover no-repeat`,
+        background: `linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0)), url(${ImgOne.src}) center / cover no-repeat`,
       }}
     >
     </div>
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Keeper from "./Pages/Keeper";
 
 export default function Home() {
   return (
     <div className="">
-       <h1>checking layout</h1>
+      <Keeper />
     </div>
   );
 }
