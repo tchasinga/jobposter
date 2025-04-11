3ce25569be42302b3f32fae07cf7b8473633b9fc
tchasinga <tchasingajacques@gmail.com>
2025-04-11 10:28:49 +0300

    Add Keeper component with background gradient and image

---
+++ app/Pages/Keeper.jsx
@@ -0,0 +1,16 @@
+"use client";
+import React from 'react';
+
+
+export default function Keeper() {
+  return (
+    <div
+      className="relative h-[550px] w-full object-fill max-w-full bg-fixed" // Added relative positioning here
+      style={{
+        background: `linear-gradient(to right, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0)), url(${Oneimg.src}) center / cover no-repeat`,
+      }}
+    >
+    </div>
+  );
+}
+
