69ff7f6def89b9436a6fddf959825f83ecb8d3fc
tchasinga <tchasingajacques@gmail.com>
2025-04-11 15:42:05 +0300

    feat: enhance Jobs component layout with improved styling and structure

--- app/Pages/Jobs.jsx
+++ app/Pages/Jobs.jsx
@@ -2,8 +2,34 @@ import React from 'react'
 
 export default function Jobs() {
   return (
-    <div>
-        
+    <div className='max-w-screen-2xl mx-auto w-full py-20'>
+         <div className='bg-slate-300'>
+
+            <div className=''>
+                <p>Show all</p>     
+            </div>
+
+            <div className=''>
+               <p>Data Visualization</p>
+            </div>
+
+            <div className=''>
+               <p>Affiale marketer</p>
+            </div>
+
+            <div className=''>
+               <p>Data/AI</p>
+            </div>
+
+            <div className=''>
+               <p>Development</p>
+            </div>
+
+            <div className=''>
+               <p>Programming</p>
+            </div>
+
+         </div>
     </div>
   )
 }
